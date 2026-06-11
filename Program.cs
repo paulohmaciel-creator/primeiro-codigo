ec1fc2e2932d1871c7a50dcd699c0091c0f23293
paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-10 21:41:38 -0300

    Impar ou Par

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Informr um numero>:!");
+int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
+if(variavelQueSalvaUmNumero % 2 == 0){  
+    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é par");
+}
+else{
+    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é impar");
+}
